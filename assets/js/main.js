2812f3568799555621dfd9d254452f92306c3537
Mahfujur Rahman <mahfujur705@gmail.com>
2018-02-22 12:29:06 +0600

    part-6
    owl-carousel and dots style

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,24 @@
         
         $('[data-toggle="tooltip"]').tooltip();
 
+        $(".frist-carousel").owlCarousel({
+        	items:1,
+        	autoplay:false,
+        	dots:false,
+        	loop:true,
+        	nav:true,
+        	navText:["<i class='fa fa-arrow-left'></i>", "<i class='fa fa-arrow-right'></i>"]
+        });
+
+        $(".product-promotion").owlCarousel({
+        	items:1,
+        	autoplay:false,
+        	dots:true,
+        	loop:true,
+        	nav:false,
+        });
+
+
 
         $(".product-list").masonry();
 
